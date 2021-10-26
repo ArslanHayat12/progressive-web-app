6cacf9827b9d98028864c3fafd2c0662022e77c7
Muhammad Talha <muhammadtalha04@gmail.com>
2021-10-26 16:23:01 +0500

    Removed stashed card border, set width to 100% and made side margins consistent

--- src/components/StackedCardCarousel/Style.ts
+++ src/components/StackedCardCarousel/Style.ts
@@ -8,7 +8,7 @@ export const StackStyle = styled.div<StackStyleProps>`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 10px 20px;
+    padding: 10px 15px;
     min-height: calc(100vh - 20px);
     ${({ containerbackground }) => `
 		background: ${containerbackground !== undefined ? containerbackground : 'none'};
