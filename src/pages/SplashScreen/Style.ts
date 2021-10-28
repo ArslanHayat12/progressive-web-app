cb7530ad422d2b05c27a1c753699251a94b83819
Muhammad Talha <muhammadtalha04@gmail.com>
2021-10-28 17:29:16 +0500

    Added animation on splash screen

--- src/pages/SplashScreen/Style.ts
+++ src/pages/SplashScreen/Style.ts
@@ -1,4 +1,16 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const loadingAnimation = keyframes`
+    0% {
+        transform: scale(0.9);
+    }
+    50% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(0.9);
+    }
+`
 
 export const SplashScreenWrapper = styled.div`
     display: grid;
@@ -6,4 +18,9 @@ export const SplashScreenWrapper = styled.div`
     height: 100vh;
     align-items: center;
     justify-content: center;
+
+    p {
+        transition: transform 1.5s linear;
+        animation: ${loadingAnimation} 1.5s infinite;
+    }
 `
